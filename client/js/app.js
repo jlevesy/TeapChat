283c8bf1e05840b016657adc2b82a2b8772c727c
Julien Levesy <jlevesy@gmail.com>
2018-03-10 11:25:21 +0100

    setup client code skeleton

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,15 @@
 (function () {
-  const exampleSocket = new WebSocket(`ws://${location.host}`, "tea-chat-protocol");
+  const conn = new WebSocket(`ws://${location.host}`, "tea-chat-protocol");
+
+  conn.onopen = () => {
+    console.log('Connecton opened !');
+  };
+
+  conn.onerror = (error) => {
+    console.log(`Connecton error ! ${error}`);
+  };
+
+  conn.onmessage = (message) => {
+    console.log(`Message received ! ${message}`);
+  };
 })();
